5083bc31cb68f9e239854778e8458cb1bb9ed1d5
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2024-03-02 14:24:46 +0530

    my npm

---
+++ notes.js
@@ -0,0 +1,12 @@
+//  we use roll up only - itbtake only small chunck so size will small
+
+// rollupjs.org
+// npm i --save-dev rollup
+//npm i --save-dev @rollup/plugin-babel
+// npm i --save-dev @rollup/plugin-typescript
+// npm i --save-dev react react-dom
+// npm i -save-dev @types/react
+
+//npm i --save-dev tslib
+// npm run build
+//after every publish hme version badana hota h har bar 1 tbhi show hoga.
---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "rollup";
+import typescript from '@rollup/plugin-typescript';
+
+export default defineConfig({
+
+    input:"src/index.ts",
+    output:{
+        dir:"dist",
+        format:"es",
+        name:"vishnu-ui",
+    },
+    external:["react","react-dom"],
+    plugins:[typescript({tsconfig:"tsconfig.json"})],
+});
